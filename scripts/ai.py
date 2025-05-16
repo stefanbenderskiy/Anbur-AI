e07ac0c224b2b3bbfa311ca5ad18f27379bb1da6
stefanbenderskiy <stefan.benderskiy@gmail.com>
2025-05-16 21:52:54 +0300

    v0.0.1

--- scripts/ai.py
+++ scripts/ai.py
@@ -1,95 +1,172 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
+
+# Функция для генерации двумерной матрицы размером x на y со случайными числами
 def generate_matrix(x, y):
     m = []
     for i in range(x * y):
         m.append(np.random.randn())
     return np.array(m).reshape(x, y)
+
+
+# Класс содержащий функции активации
 class Activation:
     def __init__(self, f, df):
         self.f = f
         self.df = df
 
+
+# Некоторые функции активации
 Activation.sigmoid = Activation(lambda x: 1 / (1 + np.exp(-x)),
                                 lambda x: np.multiply(1 / (1 + np.exp(-x)), 1 - 1 / (1 + np.exp(-x))))
 Activation.tanh = Activation(lambda x: (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x)),
-                                lambda x: 1 - np.square((np.exp(x) + np.exp(-x)) / (np.exp(x) + np.exp(-x))))
-class Layer: # Класс слоя нейронов
+                             lambda x: 1 - np.square((np.exp(x) + np.exp(-x)) / (np.exp(x) + np.exp(-x))))
+Activation.relu = Activation(lambda x: np.maximum(0, x), lambda x: 0 * x if np.maximum(0, x) == 0 else 0 * x + 1)
+Activation.linear = Activation(lambda x: x, lambda x: x / x)
+
+
+# Класс слоя нейронов
+class Layer:
     def __init__(self, weights, biases, activation=Activation.sigmoid, is_output=False):
         self.weights = weights  # веса для каждого нейрона слоя
         self.biases = biases  # смещения для каждого нейрона слоя
-        self.activation = activation # функция активации нейронов
-        self.is_output = is_output
-    def forward(self, x): # переднее распрострнение
+        self.activation = activation  # функция активации нейронов
+        self.is_output = is_output  # флаг для обозначаения выходного слоя
+
+    # Функция переднего распространения для слоя
+    def forward(self, x):
         return self.activation.f(x.dot(self.weights) + self.biases)
 
-    def backward(self, x, g, alpha = 0.05):#функция обратного распростронения для слоя
-        z,d = None, None
+    # Функция корректировки слоя (обратное распространение для слоя)
+    def backward(self, x, g, alpha=0.05):
+
+        d = None
         if self.is_output:
-            d = g
+            d = g  # в выходном слое значение градиента равно функции потери
         else:
-            z = x.dot(self.weights) + self.biases #суммирование
-            d = np.multiply(g,self.activation.df(z))#вычисление градиента для
-        g = (self.weights.dot((d.transpose()))).transpose() #вычисление градиента для последующего слоя
-        self.weights = self.weights - (alpha * x.transpose().dot(g))
-        self.biases = self.biases - (alpha * g)
+            a = self.activation.f(x.dot(self.weights) + self.biases)  # результат вычисления слоя
+            d = np.multiply(g, self.activation.df(a))  # вычисление градиента в точке a
+
+        g = (self.weights.dot((d.transpose()))).transpose()  # вычисление градиента для последующего слоя
+        self.weights = self.weights - (alpha * x.transpose().dot(d))
+        self.biases = self.biases - (alpha * d)
         return g
+
+
 class Analytics:
     def __init__(self):
         self.data = []
+
     def loss(self):
-        return [np.sum(np.square(o - y)) / np.size(o) for o,y in self.data]
+        return [np.sum(np.square(o - y)) / np.size(o) for o, y in self.data]
+
     def accuracy(self):
-        return 1 - self.loss()
+        return [1 - i for i in self.loss()]
+
     def delta(self):
-        return [np.sum(o - y) / np.size(o) for o,y in self.data]
+        return [np.sum(o - y) / np.size(o) for o, y in self.data]
+
     def total_loss(self):
         return self.loss()[-1]
+
     def total_accuracy(self):
         return self.accuracy()[-1]
+
     def total_delta(self):
         return self.delta()[-1]
+
     def average_loss(self):
-        return sum(self.loss())/self.n()
+        return sum(self.loss()) / self.n()
+
     def average_accuracy(self):
-        return sum(self.accuracy())/self.n()
-    def append(self,o,y):
-        self.data.append((o,y))
+        return sum(self.accuracy()) / self.n()
+
+    def average_delta(self):
+        return sum(self.delta()) / self.n()
+
+    def append(self, o, y):
+        self.data.append((o, y))
+
     def n(self):
         return len(self.data)
-class Perceptron: # Класс нейросети
 
+    def plot_loss(self):
+        plt.plot(range(self.n()), self.loss())
+        plt.show()
+
+    def plot_accuracy(self):
+        plt.plot(range(self.n()), self.accuracy())
+        plt.show()
+
+    def plot_delta(self):
+        plt.plot(range(self.n()), self.delta())
+        plt.show()
+
+    def __len__(self):
+        return self.n()
+
+    def __getitem__(self, i):
+        return self.data[i]
+
+    def __setitem__(self, i, o):
+        self.data[i] = o
+        return self
+
+    def __delitem__(self, i):
+        del self.data[i]
+        return self
+
+    def __contains__(self, item):
+        return item in self.data
+
+    def __iter__(self):
+        return iter(self.data)
+
+
+# Класс нейросети архитектуры Perceptron
+class Perceptron:
     def __init__(self, input_size, output_size, layers_params):
+        # input_size - размер входного слоя
+        # output_size - размер выходного слоя
+        # layers_params - параметры слоёв
         self.layers = []
-        for i in range(len(layers_params)):
+        for i in range(len(layers_params)):  # иницилизация скрытых слоёв
             size, activation = layers_params[i]
             x = 0
             if i == 0:
                 x = input_size
-
             else:
                 x = y
             y = size
-            self.layers.append(Layer(generate_matrix(x,y),generate_matrix(1,y),is_output= i==0))
-        self.layers.append(Layer(generate_matrix(y, output_size),generate_matrix(1,output_size)))
+            self.layers.append(Layer(generate_matrix(x, y), generate_matrix(1, y), activation=activation))
+        self.layers.append(
+            Layer(generate_matrix(y, output_size), generate_matrix(1, output_size),
+                  is_output=True))  # иницилизация выходного слоя
+
+    # Функция переднего распространения (прогнозирования)
     def feedforward(self, x):
         for layer in self.layers:
             x = layer.forward(x)
         return x
-    def backprop(self,x,y, alpha = 0.05):
+
+    # Функция обратного распространения (back propagation)
+    def backprop(self, x, y, alpha=0.05):
         o = self.feedforward(x)
-        g = y - o
-        a = []
+        g = o - y  # функция потери.
+        a = [x]
         for i in range(len(self.layers) - 1):
             x = self.layers[i].forward(x)
             a.append(x)
-        for i in range(len(self.layers) -1, -1, -1):
-            g = self.layers[i].backward(a[i],g,alpha)
-    def train(self,train_x,train_y, epochs = 1,learning_rate=0.05):
+        for i in range(len(self.layers) - 1, -1, -1):
+            g = self.layers[i].backward(a[i], g, alpha)
+
+    # Функция обучения нейросети
+    def train(self, train_x, train_y, epochs=1, learning_rate=0.05):
         analytics = Analytics()
         for i in range(epochs):
-            for x,y in zip(train_x,train_y):
-                self.backprop(x,y,learning_rate)
+            for x, y in zip(train_x, train_y):
+                self.backprop(x, y, learning_rate)
                 o = self.feedforward(x)
-                analytics.append(o,y)
+                analytics.append(o, y)
         return analytics
